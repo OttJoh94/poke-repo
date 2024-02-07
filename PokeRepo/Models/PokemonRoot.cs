d63643e805c3c432107532ab2b796f6c67df4656
urie8 <dionpeci08@gmail.com>
2024-02-07 11:21:11 +0100

    Added models

---
+++ PokeRepo/Models/PokemonRoot.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace PokeRepo.Models
+{
+    public class PokemonRoot
+    {
+        [Key]
+        [JsonProperty("id")]
+        public int? Id { get; set; }
+
+        [JsonProperty("forms")]
+        public List<Form> Forms { get; set; }
+
+        [JsonProperty("height")]
+        public int? Height { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("species")]
+        public Species Species { get; set; }
+
+        [JsonProperty("weight")]
+        public int? Weight { get; set; }
+    }
+}
